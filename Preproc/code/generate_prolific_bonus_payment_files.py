92f01a941e22a7bf05ccb70572f63387ba4206e0
rossspoon <rspoon@vt.edu>
2024-07-16 14:21:30 -0400

    add prolific bonus payment data

---
+++ Preproc/code/generate_prolific_bonus_payment_files.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+TEMP_DIR = 'Preproc/temp'
+PAYMENT_DIR = 'Preproc/temp/payments'
+
+p = pd.read_csv(f'{TEMP_DIR}/preproc_participant.csv')
+s = pd.read_csv(f'{TEMP_DIR}/preproc_session.csv').set_index('session')
+
+#filter out the in-lab people and set the index to session
+p = p[p.part_label.str.len() > 18].set_index('session')
+
+
+#loop sessions and generate csv files
+for sess in s.index.values:
+    p4s = p.loc[sess]
+    date = s.loc[sess, 'label']
+
+    p4s[['part_label', 'total_bonus']].to_csv(f'{PAYMENT_DIR}/session_{date}.csv', index=False, header=False)
