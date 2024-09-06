9916c9ab5c5247e4c3d7cf76d9c303d72b6533ad
cadyngo <ngocady@grinnell.edu>
2024-09-06 10:27:55 -0500

    revise label code to pull from new experiments

--- Analysis/code/prolific_participant_labels.py
+++ Analysis/code/prolific_participant_labels.py
@@ -1,11 +1,21 @@
+# Pulls ALL participant labels from the participant.csv file
+
 import pandas as pd
 
-df = pd.read_csv('/Users/cadyngo/Desktop/participant.csv')
+df1 = pd.read_csv('/Users/cadyngo/Desktop/participant.csv')
 
-participants = df['part_label'].unique()
+participants1 = df1['part_label'].unique()
 
 # filter for Prolific only
-filtered_labels = [label for label in participants if len(label) == 24]
+filtered_labels = [label for label in participants1 if len(label) == 24]
 
 filtered_labels_list = list(filtered_labels)
-print(", ".join(filtered_labels_list))
+#print(", ".join(filtered_labels_list))
+
+
+# Pulls participant labels from new experiments
+
+df2 = pd.read_csv('/Users/cadyngo/Desktop/experiment1.csv')
+
+participants2 = df2['Participant id'].unique()
+print(", ".join(participants2))
